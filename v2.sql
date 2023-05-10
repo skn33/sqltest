00fbcccd20de2735d53593e256c4a1f68646d911
skn33 <50368562+skn33@users.noreply.github.com>
2023-05-10 16:22:31 +0200

    Update v2.sql

--- v2.sql
+++ v2.sql
@@ -9,7 +9,7 @@ INSERT IGNORE INTO specialties VALUES (5, 'radiology');
 INSERT IGNORE INTO specialties VALUES (2, 'surgery');
 INSERT IGNORE INTO specialties VALUES (3, 'dentistry');
 
-INSERT IGNORE INTO vet_specialties VALUES (2, 1);
+INSERT IGNORE INTO vet_specialties VALUES (10, 1);
 INSERT IGNORE INTO vet_specialties VALUES (3, 2);
 INSERT IGNORE INTO vet_specialties VALUES (3, 3);
 INSERT IGNORE INTO vet_specialties VALUES (4, 2);
